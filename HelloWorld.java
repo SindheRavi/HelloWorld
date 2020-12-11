e228707458e8da320e51887fc80326fd015ac3d6
SindheRavi <48580826+SindheRavi@users.noreply.github.com>
2020-12-11 19:18:42 +0530

    Update HelloWorld.java
    commited new changes to auto notification of git to jenkins job

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,13 +1,7 @@
 public class HelloWorld{
 public static void main(String[] args){
 
-System.out.println("HelloWorld");
-System.out.println("HelloWorld");
-System.out.println("HelloWorld");
-System.out.println("HelloWorld");
-System.out.println("HelloWorld");
-System.out.println("HelloWorld");
-System.out.println("HelloWorld");
-System.out.println("HelloWorld");
+System.out.println("GIT AUTO COMMIT TESTING");
+  
 }
 }
